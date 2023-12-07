8a0b2fdbd8f52a7c2c1a38f9d9a094f8cc191114
Monteiro <monteirolnx@outlook.com>
2023-12-07 17:05:50 -0300

    Instruções mais claras.

--- codigo_fonte/TesteV2/Program.cs
+++ codigo_fonte/TesteV2/Program.cs
@@ -17,8 +17,8 @@ return;
 
 void Tarefa01()
 {
-    // Tarefa 01 - Quando invoco o serviço EmpresaService para adicionar quatro empresas, o programa deixa incluir CNPJ repetido.
-    // Qual seria a solução para não permitir inserção de CNPJ repetido?
+    //Tarefa 01: Ao utilizar o serviço EmpresaService para registrar quatro empresas, foi observado que o método Adicionar permite a inclusão de CNPJs duplicados na lista.
+    //Como posso modificar este método para impedir a inserção de CNPJs repetidos?
     empresaService.Adicionar(new Empresa("Empresa BB", "00.000.000/0001-91", TipoSituacao.Ativo, 1000000));
     empresaService.Adicionar(new Empresa("Empresa Alpha", "12.345.678/0001-99", TipoSituacao.Ativo, 1000000));
     empresaService.Adicionar(new Empresa("Empresa Beta", "98.765.432/0001-88", TipoSituacao.Inativo, 500000));
@@ -30,37 +30,37 @@ void Tarefa01()
 
 void Tarefa02()
 {
-    // Tarefa 02 - Chamar o serviço EmpresaService e buscar a empresa com CNPJ 12.345.678/0001-99.
+    //Tarefa 02-A: Utilizar o serviço EmpresaService para para localizar e obter informações da empresa cujo CNPJ é 12.345.678/0001-99.
     Console.WriteLine("Resultado tarefa 02:");
-    
-    //Imprimir o nome da empresa encontrada.
+
+    //Tarefa 02-B:Imprimir o nome da empresa encontrada.
     Console.WriteLine(Environment.NewLine);
 }
 
 void Tarefa03()
 {
-    // Tarefa 03 - Chamar o serviço EmpresaService e atualizar a empresa com CNPJ 12.345.678/0001-99 para a situação Inativo.
+    //Tarefa 03: Utilizar o serviço EmpresaService para alterar o status da empresa com CNPJ 12.345.678/0001-99 para 'Inativo'.
     Console.WriteLine("Resultado tarefa 03:");
     empresaService.ImprimirInformacoes();
 }
 
 void Tarefa04()
 {
-    // Tarefa 04 - Listar todas as empresas INATIVAS e imprimir na tela.
+    //Tarefa 04: Listar e exibir em tela todas as empresas com status 'Inativo'.
     Console.WriteLine("Resultado tarefa 04:");
 }
 
 void Tarefa05()
 {
-    // Tarefa 05 - Chamar o serviço EmpresaService e remover a empresa com CNPJ 98.765.432/0001-88.
+    //Tarefa 05: Utilizar o serviço EmpresaService para remover a empresa com CNPJ 98.765.432/0001-88.
     Console.WriteLine("Resultado tarefa 05:");
     empresaService.ImprimirInformacoes();
 }
 
 void Tarefa06()
 {
-    //Tarefa 06 - A impressão do faturamento anual está sem formato de moeda.
-    //Como você faria para utilizar o método FormatarMoeda da classe Util para imprimir o faturamento anual?
+    //Tarefa 06: A exibição do faturamento anual está sem o formato de moeda.
+    //Qual seria a melhor maneira de incorporar o método FormatarMoeda da classe Util (localizada na pasta 'Utils') ao método ImprimirInformacoes?"
     Console.WriteLine("Resultado tarefa 06:");
     empresaService.ImprimirInformacoes();
 }
